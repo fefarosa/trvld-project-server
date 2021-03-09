11b8d536003171ee29eb7b558bfcd3ccbd1a7c0f
EduLp99 <edulp99@gmail.com>
2021-03-09 16:03:39 -0300

    att  config

--- src/index.js
+++ src/index.js
@@ -28,8 +28,11 @@ app.use(express.json());
 
 // Router
 const logs = require("./routes/post.routes.js");
+const user = require("./routes/user.routes.js");
+
 
 app.use("/", logs);
+app.use("/", user);
 
 // Error handlers middlewares :)
 app.use(middlewares.notFound);
